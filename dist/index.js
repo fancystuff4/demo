67b70a98b584020934780b7d9280312c7e969a30
fancystuff4 <fancystuff4@gmail.com>
2023-06-10 22:51:01 +0530

    chore(lint): ran prettier against the release workflow file

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./your-package-name-you-want-to-see-on-npm-lol.cjs.production.min.js')
+  module.exports = require('./fancystuff4.cjs.production.min.js')
 } else {
-  module.exports = require('./your-package-name-you-want-to-see-on-npm-lol.cjs.development.js')
+  module.exports = require('./fancystuff4.cjs.development.js')
 }
